28b9931cede085e0240425abbf40b8f5bd278523
DerekJ <derekj@microsoft.com>
2014-11-21 10:22:41 -0800

    put project files in subfolder

---
+++ ClownChase/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using Microsoft.Kinect;
+
+namespace ClownChase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private IKinect _kinect;
+        private KinectSensor _sensor;
+
+        public MainWindow()
+        {
+            _kinect = new Kinect();
+            InitializeComponent();
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = _kinect.GetSensor();
+            if (_sensor == null)
+            {
+                ShowStatus(Properties.Resources.KinectNotFound);
+                return;
+            }
+
+            ShowStatus(Properties.Resources.KinectFound);
+        }
+
+        private void ShowStatus(string message)
+        {
+            StatusBarText.Text = message;
+        }
+    }
+}
